fd4c186e1f6fa02d3a412bf77c7fdd5aa7158433
Ahror <Ahror@Lenovo.com>
2021-10-19 11:36:43 +0300

    Split card and scroll animations

---
+++ script.js
@@ -0,0 +1,15 @@
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkboxes);
+function checkboxes() {
+  const bottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < bottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
